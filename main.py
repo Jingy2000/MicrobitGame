339d4c39d2e81eb8996e7d634cc2e6e39e2d3bdb
HJX <1248965277@qq.com>
2019-04-18 21:15:55 +0800

    声明了主程序调用主函数，新建了display.py

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 from player_and_bullet import *
+
+
 # ----------------------------MAIN--------------------------------
+def main():
+    player_red = Player(0)
+    player_blue = Player(100)  # 地图高度这个参数
+    bullet_list_red = []
+    bullet_list_blue = []
 
-player_red = Player(0)
-player_blue = Player(100) # 地图高度这个参数
-bullet_list_red = []
-bullet_list_blue = []
+    while True:
+        # 接受指令
+        pass
 
-while True:
-    # 接受指令
-    pass
+        # 人物移动和射击
+        pass
 
-    # 人物移动和射击
-    pass
+        # 子弹的运动
+        for bullet in bullet_list_red:
+            bullet.move()
+        for bullet in bullet_list_blue:
+            bullet.move()
 
-    # 子弹的运动
-    for bullet in bullet_list_red:
-        bullet.move()
-    for bullet in bullet_list_blue:
-        bullet.move()
+        # 子弹出屏判定
+        pass
 
-    # 子弹出屏判定
-    pass
+        # *子弹之间的碰撞判定
+        pass
 
-    # *子弹之间的碰撞判定
-    pass
+        # *子弹存活判定(?)
+        pass
 
-    # *子弹存活判定(?)
-    pass
+        # 子弹与人物的碰撞判定
+        for bullet in bullet_list_red:
+            if bullet.hit(player_blue):
+                bullet_list_red.remove(bullet)
+        for bullet in bullet_list_blue:
+            if bullet.hit(player_red):
+                bullet_list_blue.remove(bullet)
 
-    # 子弹与人物的碰撞判定
-    for bullet in bullet_list_red:
-        if bullet.hit(player_blue):
-            bullet_list_red.remove(bullet)
-    for bullet in bullet_list_blue:
-        if bullet.hit(player_red):
-            bullet_list_blue.remove(bullet)
-
-    # 调用绘图
-    pass
+        # 调用绘图
+        pass
 
 
+# 当前程序为主程序时调用
+if __name__ == "__main__":
+    main()
